e5c6aba55c1d46e5638e8e7f474f1ff9f11616d7
santiagoalex <santiagos9511@gmail.com>
2020-04-27 15:46:55 -0500

    Usind a property binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent{
     pageTitle: string = 'Product List!';
+    imageWidth: number =50;
+    imageMargin: number= 2;
     products: any[] =[ 
       {
         "productId": 2,
@@ -15,7 +17,8 @@ export class ProductListComponent{
         "description": "15 gallon capacity rolling garden cart",
         "price": 32.99,
         "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
+        "imageUrl": "/assets/images/garden_cart.png"
+       
       },
       {
         "productId": 5,
