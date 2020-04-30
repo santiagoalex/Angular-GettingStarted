ef2821baf08da267c7ba9f1bdfd30d787ebb2eea
santiagoalex <santiagos9511@gmail.com>
2020-04-30 11:34:30 -0500

    Add a root sevice

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -24,27 +24,6 @@ export class ProductListComponent implements OnInit{
 
     filteredProducts:IProduct[];
     products: IProduct[] =[ 
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2019",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "/assets/images/garden_cart.png"
-       
-      },
-      {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2019",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "assets/images/hammer.png"
-      }
     ]; 
     
     constructor (){
